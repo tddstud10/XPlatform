6657afd7211571eb0678ee1e8e77165782295d0f
Partho P. Das <parthopdas@live.com>
2017-06-07 15:18:50 +1000

    Loading dependencies from current folder

--- R4nd0mApps.XTestPlatform.XUnit/AdapterLoader.fs
+++ R4nd0mApps.XTestPlatform.XUnit/AdapterLoader.fs
@@ -1,9 +1,20 @@
 ﻿namespace R4nd0mApps.XTestPlatform.Implementation.XUnit
 
 open R4nd0mApps.XTestPlatform.Api
+open System.Reflection
+open System.IO
 
 module AdapterLoader = 
+    let private loadDependencies () = 
+        [ "xunit.abstractions.dll"; "xunit.runner.utility.net35.dll" ]
+        |> List.map (Prelude.tuple2 (Path.getLocalPath()) >> Path.Combine)
+        |> List.iter (Assembly.LoadFrom >> ignore)
+
     let LoadDiscoverers : string -> seq<IXTestDiscoverer> = 
-        fun _ -> [XUnitTestDiscoverer() :> IXTestDiscoverer] :> seq<_>
+        fun _ -> 
+            loadDependencies ()
+            [XUnitTestDiscoverer() :> IXTestDiscoverer] :> seq<_>
     let LoadExecutors : string -> seq<IXTestExecutor> = 
-        fun _ -> [XUnitTestExecutor() :> IXTestExecutor] :> seq<_>
+        fun _ -> 
+            loadDependencies ()
+            [XUnitTestExecutor() :> IXTestExecutor] :> seq<_>
